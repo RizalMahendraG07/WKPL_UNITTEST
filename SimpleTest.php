9d14880127fd0b87d91e748da06d4f031a105c80
rizalmahendrag07 <71058828+RizalMahendraG07@users.noreply.github.com>
2023-10-20 20:51:10 +0700

    Update SimpleTest.php

--- SimpleTest.php
+++ SimpleTest.php
@@ -10,13 +10,15 @@ class SimpleTest extends TestCase
 {
     public  function testCountWords()
     {
-        // Kita pakai class yang mau kita test
+        // Kita membuat class yang mau kita test dengan kode dibawah
+        // 
         $wc = new WordCount();
-        // kita masukkan parameter 4 kata, yang harusnya dapat output 4.
+        // kita masukkan parameter untuk variabel testSentence 4 kata, yang harusnya dap    at output 4.
         $testSentence = "My Name is Rizal"; // 4kata
         $WordCount = $wc->countWords($testSentence);
 
-        // kita assert equal, ekspektasi nya harus 4, jikabenar berarti wordcount berfungsi dengan baik
+        // kita assert equal, ekspektasi nya harus 4, jika benar berarti wordcount berfungsi dengan baik
+        
         $this->assertEquals(4, $WordCount);
     }
 
