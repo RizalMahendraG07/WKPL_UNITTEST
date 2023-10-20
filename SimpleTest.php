8cfa3154cc10e1d1d16a4823bf702d962c31f485
rizalmahendrag07 <71058828+RizalMahendraG07@users.noreply.github.com>
2023-10-20 21:07:27 +0700

    Update SimpleTest.php

--- SimpleTest.php
+++ SimpleTest.php
@@ -1,9 +1,11 @@
 <?php
 // path to run./vendor/bin/phpunit -- bootstrap vendor/autoload.php Filename.php
 // Butuh Framework PHPUnit
-use PHPUnit\Framework\TestCase;
+
+use PHPUnit\Framework\TestCase; //mengimpor framework testcase yang ada didalam phpunit
 
 // class yang mau ditest wordcount.php
+// menggunakan require_once yang berfungsi memanggil sekali saja
 require_once "wordcount.php";
 // class  untuk run testing
 class SimpleTest extends TestCase
@@ -18,7 +20,8 @@ class SimpleTest extends TestCase
         $WordCount = $wc->countWords($testSentence);
 
         // kita assert equal, ekspektasi nya harus 4, jika benar berarti wordcount berfungsi dengan baik
-        
+        //Variabel $this merupakan variabel khusus PHP dalam OOP yang digunakan untuk menunjuk sebuah objek, 
+        //ketika kita mau mengaksesnya dalam sebuah class. $this juga sering disebut dengan pseuo-variable
         $this->assertEquals(4, $WordCount);
     }
 
