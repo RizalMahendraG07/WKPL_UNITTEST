7b15301ebf2d37ec8cd8fdb629e3cb214a736fe6
rizalmahendrag07 <71058828+RizalMahendraG07@users.noreply.github.com>
2023-10-20 20:55:03 +0700

    Update wordcount.php

--- wordcount.php
+++ wordcount.php
@@ -1,6 +1,8 @@
 <?php 
 class WordCount {
+ //membuat fungsi countwords dengan parameter variabel sentece
  public function countWords($sentence) {
+  //fungsi explode adalah untuk memisahkan string dan string yang sudah dipecah akan menjadi array
     return count(explode(" ",$sentence));
  }
 
